7619ab1efe5a44f8e6fcbe2dbff1f5fa8836e2b5
Pratik Labh <pratiklabh10@gmail.com>
2024-08-15 12:14:17 +0545

    added the created at and updated at functionality

--- src/main/java/com/syntech/sbs/model/BaseIdEntity.java
+++ src/main/java/com/syntech/sbs/model/BaseIdEntity.java
@@ -1,18 +1,39 @@
 package com.syntech.sbs.model;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 
 @MappedSuperclass
-public abstract class BaseIdEntity implements Serializable{
+public abstract class BaseIdEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+    
+    @PrePersist
+    private void created(){
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    private void updated(){
+        updatedAt= LocalDateTime.now();
+    }
+    
     public Long getId() {
         return id;
     }
@@ -20,4 +41,22 @@ public abstract class BaseIdEntity implements Serializable{
     public void setId(Long id) {
         this.id = id;
     }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+    
+    
 }
