9b229839aa081c9ee370ad4aa8188cb1ee1c0004
Pratik Labh <pratiklabh10@gmail.com>
2024-08-28 11:56:59 +0545

    Added validation annotations in sales and salesdetails

--- src/main/java/com/syntech/sbs/model/Sales.java
+++ src/main/java/com/syntech/sbs/model/Sales.java
@@ -10,25 +10,34 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "sales")
 public class Sales extends BaseIdEntity {
 
-    @ManyToOne
+     @ManyToOne
     @JoinColumn(name = "customer_id", nullable = false)
+    @NotNull(message = "Customer cannot be null")
     private User customer;
 
     @Column(name = "date", nullable = false)
+    @NotNull(message = "Date cannot be null")
     private LocalDateTime date;
 
     @Column(name = "total", nullable = false)
+    @NotNull(message = "Total cannot be null")
+    @Min(value = 0, message = "Total must be zero or greater")
     private BigInteger total;
 
     @Column(name = "payment_mode", nullable = false)
+    @NotNull(message = "Payment mode cannot be null")
+    @Size(min = 1, max = 50, message = "Payment mode must be between 1 and 50 characters")
     private String paymentMode;
 
-    @OneToMany(mappedBy = "sales", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "sales", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<SalesDetails> salesDetails;
 
     public Sales() {
--- src/main/java/com/syntech/sbs/model/SalesDetails.java
+++ src/main/java/com/syntech/sbs/model/SalesDetails.java
@@ -6,6 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "sales_details")
@@ -13,22 +16,32 @@ public class SalesDetails extends BaseIdEntity {
 
     @ManyToOne
     @JoinColumn(name = "sales_id", nullable = false)
+    @NotNull(message = "Sales cannot be null")
     private Sales sales;
 
     @ManyToOne
     @JoinColumn(name = "product_id", nullable = false)
+    @NotNull(message = "Product cannot be null")
     private Product product;
 
     @Column(name = "quantity", nullable = false)
+    @NotNull(message = "Quantity cannot be null")
+    @Min(value = 1, message = "Quantity must be greater than zero")
     private Integer quantity;
 
     @Column(name = "rate", nullable = false)
+    @NotNull(message = "Rate cannot be null")
+    @Min(value = 1, message = "Rate must be greater than zero")
     private BigInteger rate;
 
     @Column(name = "discount", nullable = false)
+    @NotNull(message = "Discount cannot be null")
+    @Min(value = 0, message = "Discount must be zero or greater")
     private BigInteger discount;
 
     @Column(name = "unit", nullable = false)
+    @NotNull(message = "Unit cannot be null")
+    @Size(min = 1, max = 50, message = "Unit must be between 1 and 50 characters")
     private String unit;
 
     public SalesDetails() {
