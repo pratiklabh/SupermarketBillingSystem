369c9fb707428cd98ffcc75d79f6b9836f38ee4a
Pratik Labh <pratiklabh10@gmail.com>
2024-08-29 10:26:55 +0545

    Removed the p:messages from product, user and supplier form and updated the userRepo test for correct testing of methods find by username, email, phone, usernameAndPassword

--- src/test/java/com/syntech/sbs/repository/UserRepositoryTest.java
+++ src/test/java/com/syntech/sbs/repository/UserRepositoryTest.java
@@ -1,16 +1,14 @@
 package com.syntech.sbs.repository;
 
 import com.syntech.sbs.model.User;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
-
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 
 public class UserRepositoryTest {
@@ -58,10 +56,68 @@ public class UserRepositoryTest {
         when(typedQuery.getSingleResult()).thenReturn(user1);  // Return user1 when getSingleResult is called
     }
 
+//    @Test
+//    public void testSave() {
+//        User u = new User("Test", "test", "test@123", "test@gmail.com", "0987654321", "Admin", "Active");
+//        userRepo.save(u);
+//        assertEquals(1, userRepo.findAll().size());
+//    }
+
+//    @Test
+//    public void testDelete() {
+//        User u = new User("Test", "test", "test@123", "test@gmail.com", "0987654321", "Admin", "Active");
+//        userRepo.save(u);
+//        assertEquals(1L, userRepo.findAll().size());
+//        userRepo.delete(u.getId());
+//        assertEquals(0, userRepo.findAll().size());
+//    }
+//    @Test
+//    public void testUpdate() {
+//        User u = new User("Test", "test", "test@123", "test@gmail.com", "0987654321", "Admin", "Active");
+//        userRepo.save(u);
+//        u.setName("Messiiiiiii");
+//        userRepo.update(u);
+//        
+//    }
+
+//    @Test
+//    public void testFindById() {
+//        User u = new User("Pratik", "pratik", "pratik@123", "pratik@gmail.com", "0987654321", "Admin", "Active");
+//        userRepo.save(u);
+//        assertEquals("Pratik", userRepo.findById(u.getId()).getName());
+//        
+//    }
+    
+//    @Test
+//    public void testFindAll(){
+//        User u = new User("Test", "test", "test@123", "test@gmail.com", "0987654321", "Admin", "Active");
+//        userRepo.save(u);
+//        assertEquals(1, userRepo.findAll().size());
+//    }
+
     @Test
     public void testFindByUsernameAndPassword() {
         User user = userRepo.findByUsernameAndPassword("pratik", "pratik@123");
         assertNotNull(user);
         assertEquals("pratik@example.com", user.getEmail());
     }
+
+    @Test
+    public void testFindByUsername() {
+        User user = userRepo.findByUsername("pratik");
+        assertEquals("1234567890", user.getPhone());
+    }
+
+    @Test
+    public void testFindByPhone() {
+        User user = userRepo.findByPhone("1234567890");
+        assertEquals("pratik", user.getUsername());
+    }
+
+    @Test
+    public void testFindByEmail() {
+        User user = userRepo.findByEmail("pratik@example.com");
+        assertEquals("1234567890", user.getPhone());
+    }
+
 }
